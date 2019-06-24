e13eb219d81cc63830d9511ca4539cb7fc2c468c
Kris <krisdixon@yahoo.com>
2019-06-24 12:01:00 -0500

    second draft

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -9,7 +9,7 @@ namespace Text_Adventure
             string name;
             Console.WriteLine("Welcome to the Burger Barn! What is your name?");
             name = Console.ReadLine();
-            Console.WriteLine("Welcome {0}, we have 4 burgers to choose from.", name);
+            Console.WriteLine(name + ", Welcome! We have 4 burgers to choose from.");
             Console.WriteLine("We have a bacon cheeseburger, vegetarian, triple meat cheeseburger, The Works.");
             Console.WriteLine("Which one would you like:");
             string response;
@@ -17,24 +17,25 @@ namespace Text_Adventure
 
             if (response == "bacon cheeseburger")
             {
-                Console.WriteLine("{0}, You will enjoy the 5 pieces of apple wood smoked bacon we use.", name);
+                Console.WriteLine(name + ", you will enjoy the 5 pieces of apple wood smoked bacon we use." );
             }
             else if (response == "vegetarian")
             {
-                Console.WriteLine("{0}, We have a meatless patty made from black beans and mushrooms.", name);
+                Console.WriteLine(name + ", we have a meatless patty made from black beans and mushrooms.");
                 Console.WriteLine("Our hippie customers really love it!");
             }
             else if (response == "triple meat cheeseburger")
             {
-                Console.WriteLine("You will need to see a cardiologist after eating this.");
+                Console.WriteLine("You will need to see a cardiologist after eating this burger.");
             }
             else if (response == "The Works")
             {
-                Console.WriteLine("This has 3 patties, triple cheese, onions, lettuce and our secret sauce");
+                Console.WriteLine("This has 3 patties, triple cheese, onions, lettuce and our secret sauce.");
+                Console.WriteLine("It is our top seller.");
             }
 
             string input;
-            Console.WriteLine("{0}, Did you enjoy your burger?", name);
+            Console.WriteLine(name + ", Did you enjoy your burger?");
             input = Console.ReadLine();
                                 
         
